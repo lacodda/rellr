da7f47598b55251b2b85b459f54ac531f156fb91
Kirill Lakhtachev <lahtachev@gmail.com>
2024-01-19 10:43:14 +0100

    feat: The "to_path_str" function has been added to the "Helpers" module to convert a vector of strings to a path string

--- src/libs/helpers.rs
+++ src/libs/helpers.rs
@@ -1,6 +1,13 @@
 use path_absolutize::Absolutize;
+use regex::Regex;
 use std::path::Path;
 
 pub fn to_abs_path(path: &str) -> String {
     Path::new(&path).absolutize().unwrap().to_str().unwrap().to_string()
+}
+
+pub fn to_path_str(parts: Vec<&str>) -> String {
+    let path_str = parts.join("/").replace("\\", "/");
+    let regex = Regex::new(r"^(\.?\/)+").unwrap();
+    regex.replace(&path_str, "").to_string()
 }
